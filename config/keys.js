5e8a6903b247c3b13a27d4def59753eaa2b6016e
hryniu1984 <piotr.hryniewicz84@gmail.com>
2018-04-14 02:12:52 +0200

    started UI

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://hryniu1984:hryniu1984@ds129906.mlab.com:29906/storybooks-dev',
+  googleClientID:'105556866753-30mpc8bphqaeqvpdmjsl3uls10vq7fdi.apps.googleusercontent.com',
+  googleClientSecret:'fkXJw7a-YtLYy_AQdgqmPUnU'
+}
