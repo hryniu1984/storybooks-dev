2e230b552e1e027fab8c7a97ecc5e530ad369d7a
hryniu1984 <piotr.hryniewicz84@gmail.com>
2018-04-25 04:41:34 +0200

    version 0.9.9beta

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:'mongodb://hryniu1984:hryniu1984@ds129906.mlab.com:29906/storybooks-dev',
-  googleClientID:'105556866753-30mpc8bphqaeqvpdmjsl3uls10vq7fdi.apps.googleusercontent.com',
-  googleClientSecret:'fkXJw7a-YtLYy_AQdgqmPUnU'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
